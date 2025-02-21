0a3f7cb0ad07109e17b07914e83d04e02097874b
bk-git-hub <bkprivate99@gmail.com>
2025-02-21 13:09:25 +0900

    test: add error message test for SignInForm

---
+++ tests/__mocks/formdata.ts
@@ -0,0 +1,4 @@
+export const validEmail = 'test@email.com';
+export const invalidEmail = 'invalidEmail';
+
+export const validPassword = 'password123!';
--- tests/unit/SignInForm.test.tsx
+++ tests/unit/SignInForm.test.tsx
@@ -1,5 +1,6 @@
 import SignInForm from '@/components/auth/SignInForm';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { validEmail, invalidEmail, validPassword } from '../__mocks/formdata';
 
 describe('SignInForm', () => {
   test('rendering test', () => {
@@ -8,5 +9,36 @@ describe('SignInForm', () => {
     expect(screen.getByText(/이메일/i)).toBeInTheDocument();
     expect(screen.getByText(/비밀번호/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /로그인/i })).toBeInTheDocument();
+  });
+
+  test('updates email and password field on user input', () => {
+    render(<SignInForm />);
+    const emailInput = screen.getByLabelText(/이메일/i) as HTMLInputElement;
+    const passwordInput = screen.getByLabelText(
+      /비밀번호/i
+    ) as HTMLInputElement;
+
+    fireEvent.change(emailInput, { target: { value: validEmail } });
+    fireEvent.change(passwordInput, { target: { value: validPassword } });
+
+    expect(emailInput.value).toBe(validEmail);
+    expect(passwordInput.value).toBe(validPassword);
+  });
+
+  test('displays error message for invalid inputs', async () => {
+    render(<SignInForm />);
+    const emailInput = screen.getByLabelText(/이메일/i) as HTMLInputElement;
+
+    const submitButton = screen.getByRole('button', { name: /로그인/i });
+
+    fireEvent.change(emailInput, { target: { value: invalidEmail } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/유효하지 않은 이메일 형식입니다/i)
+      ).toBeInTheDocument();
+      expect(screen.getByText(/비밀번호를 입력해주세요/i)).toBeInTheDocument();
+    });
   });
 });
