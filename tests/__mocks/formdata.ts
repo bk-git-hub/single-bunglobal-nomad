b3e02196e23ad2fcc97a6b91b1c22d61b02e60e8
bk-git-hub <bkprivate99@gmail.com>
2025-02-21 18:01:15 +0900

    test: add more vaild and invalid mock datas for testing

--- tests/__mocks/formdata.ts
+++ tests/__mocks/formdata.ts
@@ -2,3 +2,7 @@ export const validEmail = 'test@email.com';
 export const invalidEmail = 'invalidEmail';
 
 export const validPassword = 'password123!';
+export const invalidPassword = 'pas12';
+
+export const validNickName = 'BK99';
+export const invalidNickName = 'B';
