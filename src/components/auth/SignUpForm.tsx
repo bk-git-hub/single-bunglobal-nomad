d629aa17f98e7dc4b0eb751a7b0b56bdfc0b7f34
bk-git-hub <bkprivate99@gmail.com>
2025-02-21 18:33:10 +0900

    feat: create SignUpForm and page

---
+++ src/components/auth/SignUpForm.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import AuthPageInput from './AuthPageInput';
+import PasswordInput from './PasswordInput';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+const signUpSchema = z
+  .object({
+    email: z.string().email('유효하지 않은 이메일입니다'),
+    nickname: z.string().min(2, '닉네임은 최소 2글자 이상입니다'),
+    password: z.string().min(8, '비밀번호는 최소 8자리 이상입니다'),
+    passcheck: z.string(),
+  })
+  .refine((data) => data.password === data.passcheck, {
+    message: '비밀번호가 일치하지 않습니다', // Custom error message
+    path: ['passcheck'], // Attach the error to the passcheck field
+  });
+
+type SignUpFormValues = z.infer<typeof signUpSchema>;
+
+export default function SignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormValues>({ resolver: zodResolver(signUpSchema) });
+
+  const onSubmit = (data: SignUpFormValues) => {
+    console.log('Form data:', data);
+  };
+
+  return (
+    <div className='flex flex-col gap-[40px] items-center w-[640px]'>
+      <Link href={'/'}>
+        <Image
+          src={'/GlobalNomadIcon.svg'}
+          alt='global nomad'
+          width={340}
+          height={192}
+        />
+      </Link>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='w-full flex flex-col gap-7'
+      >
+        <AuthPageInput
+          labelText='이메일'
+          idText='email'
+          register={register('email')}
+          placeholder='example@email.com'
+          errorMsg={errors.email ? errors.email.message : ''}
+        />
+        <AuthPageInput
+          labelText='닉네임'
+          placeholder='최소 2자 이상 입력바랍니다'
+          idText='nickname'
+          register={register('nickname')}
+          errorMsg={errors.nickname ? errors.nickname.message : ''}
+        />
+        <PasswordInput
+          labelText='비밀번호'
+          id='password'
+          register={register('password')}
+          errorMsg={errors.password ? errors.password.message : ''}
+        />
+        <PasswordInput
+          labelText='비밀번호 확인'
+          id='passcheck'
+          register={register('passcheck')}
+          errorMsg={errors.passcheck ? errors.passcheck.message : ''}
+        />
+        <button
+          type='submit'
+          className='mt-7 py-4 bg-primary text-white rounded-[6px]'
+        >
+          회원가입
+        </button>
+      </form>
+
+      <div className='flex gap-4 items-center'>
+        <span>회원이신가요?</span>
+        <Link href={'/signin'} className='underline text-[20px]'>
+          로그인하기
+        </Link>
+      </div>
+    </div>
+  );
+}
