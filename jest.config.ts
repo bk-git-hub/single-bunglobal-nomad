760ea81de2dbf1b4d4fccdcbe1822499e6203806
bk-git-hub <bkprivate99@gmail.com>
2025-02-21 12:21:02 +0900

    test: fix SignInForm test and jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,19 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-  testEnvironment: 'node', // Use 'jsdom' for frontend projects
+  testEnvironment: 'jsdom', // Use 'jsdom' for frontend projects
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'], // Look for test files
   coverageDirectory: 'coverage', // Output coverage reports here
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   collectCoverage: true, // Enable coverage reporting
   transform: {
     '^.+\\.tsx?$': 'ts-jest', // Use ts-jest to transform TypeScript files
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Adjust path based on your project structure
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
 };
 
 export default config;
--- tests/unit/SignInForm.test.tsx
+++ tests/unit/SignInForm.test.tsx
@@ -5,8 +5,8 @@ describe('SignInForm', () => {
   test('rendering test', () => {
     render(<SignInForm />);
 
-    expect(screen.getAllByText(/이메일/i)).toBeInTheDocument();
-    expect(screen.getAllByText(/비밀번호/i)).toBeInTheDocument();
+    expect(screen.getByText(/이메일/i)).toBeInTheDocument();
+    expect(screen.getByText(/비밀번호/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /로그인/i })).toBeInTheDocument();
   });
 });
