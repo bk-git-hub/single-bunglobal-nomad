4283de1b5b3cccca2925033b0405b22339301b3c
bk-git-hub <bkprivate99@gmail.com>
2025-02-24 12:48:15 +0900

    feat: create Header component

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,30 @@
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default async function Header() {
+  const session = await getServerSession(authOptions);
+  if (session) console.log(session);
+  return (
+    <header className='flex w-full items-center justify-between max-w-[1200px] p-5'>
+      <Image
+        src={'/HeaderLogo.svg'}
+        width={160}
+        height={28}
+        alt='global nomad'
+      />
+
+      {!session ? (
+        <div className='flex gap-6'>
+          <Link href={'/signin'}>로그인</Link>
+
+          <Link href={'/signup'}>회원가입</Link>
+        </div>
+      ) : (
+        <div className='flex'>{session.user.name}</div>
+      )}
+    </header>
+  );
+}
